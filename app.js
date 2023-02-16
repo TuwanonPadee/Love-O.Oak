4d664dc56c49804c69c90742fe6ab299682db07b
PalmTon <6352300421@stu.pim.ac.th>
2023-02-17 01:39:16 +0700

    create mongoose query from mongoDB databse and config folder project

--- app.js
+++ app.js
@@ -1,15 +1,26 @@
-const { MongoClient, ServerApiVersion } = require("mongodb");
-const uri =
-  "mongodb+srv://tuwanon:<password>@cluster0.qew2hux.mongodb.net/?retryWrites=true&w=majority";
-const client = new MongoClient(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  serverApi: ServerApiVersion.v1,
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+
+const app = express();
+const Schema = mongoose.Schema;
+
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.listen(3000, () => {
+  console.log("Server opened on port 3000");
 });
-client.connect((err) => {
-  const collection = client.db("test");
-  const product = collection.databaseName;
-  console.log(product);
-  // perform actions on the collection object
-  client.close();
-});
+
+mongoose.set("strictQuery", true);
+mongoose.connect(
+  "mongodb+srv://tuwanon:Q4lyU7QGdtRdtLwG@cluster0.qew2hux.mongodb.net/shop?retryWrites=true&w=majority"
+);
+
+mongoose
+  .model("product", new Schema({ id: Number, tags: String }), "product")
+  .find({ tags: "romance novels" }, (err, query) => {
+    console.log(query);
+  });
