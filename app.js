719e79ffe21384fba07c5ce626238e47975bc284
TonPalm Tuwanon <6352300421@stu.pim.ac.th>
2023-02-17 22:42:56 +0700

    add route and create get value on web template

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
 const express = require("express");
-const bodyParser = require("body-parser");
-const ejs = require("ejs");
-const mongoose = require("mongoose");
-
 const app = express();
-const Schema = mongoose.Schema;
 
-app.use(express.static("public"));
 app.set("view engine", "ejs");
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
 app.listen(3000, () => {
   console.log("Server opened on port 3000");
 });
 
+app.get("/shop/:tags", async (req, res) => {
+  await res.render("main", { checkPath: req.params.tags });
+});
+
+/* 
+// const mongoose = require("mongoose");
 mongoose.set("strictQuery", true);
 mongoose.connect(
   "mongodb+srv://tuwanon:Q4lyU7QGdtRdtLwG@cluster0.qew2hux.mongodb.net/shop?retryWrites=true&w=majority"
@@ -23,4 +23,4 @@ mongoose
   .model("product", new Schema({ id: Number, tags: String }), "product")
   .find({ tags: "romance novels" }, (err, query) => {
     console.log(query);
-  });
+  }); */
