7131f85af7fd409b6af3b293afd1a1ad845c2072
TuwanonPadee <113566601+TuwanonPadee@users.noreply.github.com>
2023-02-20 23:01:14 +0700

    refactor : create add route for detail to product

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ const express = require("express");
 const Book = require("./models/book");
 const app = express();
 
+const tagConvert = {
+  "romance-story": "romance novels",
+  "love-story": "love story",
+  "boylove-story": "y-boylove",
+};
+
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
@@ -10,16 +16,29 @@ app.listen(3000, () => {
 });
 
 app.get("/shop/:tags", async (req, res) => {
-  const tag = req.params.tags,
-    tagConvert = {
-      "romance-story": "romance novels",
-      "love-story": "love story",
-      "boylove-story": "y-boylove",
-    };
+  const tag = req.params.tags;
+
   const response = await Book.find({ tags: tagConvert[tag] });
   res.render("main", {
     checkPath: tag,
+    bookProduct: {},
+    bookProductList: response,
+    tagMock: tagConvert[tag],
+  });
+});
+
+app.get("/shop/:tag/detail/:id", async (req, res) => {
+  const paramTag = req.params.tag,
+    paramId = req.params.id;
+
+  const response = await Book.findOne({
+    id: paramId,
+    tags: tagConvert[paramTag],
+  });
+  res.render("main", {
+    checkPath: "detail",
     bookProduct: response,
-    tagMock: tagConvert[tag]
+    bookProductList: [],
+    tagMock: "",
   });
 });
